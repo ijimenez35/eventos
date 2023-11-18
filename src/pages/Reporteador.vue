5281e9445d96957f6085a2620e8d7d3a2c161d58
Israel <ijimenez35@gmail.com>
2023-11-17 22:13:28 -0600

    se agrega cambio solicitado

--- src/pages/Reporteador.vue
+++ src/pages/Reporteador.vue
@@ -106,9 +106,49 @@
                   </div>
                 </div>
               </div>
+
+              <div class="row" v-show="baseDatos.length > 0">
+                <div class="col-md-12">
+                  <h5 class="card-title">Listado de Bases de Datos</h5>
+                  <table class="table table-hover table-striped">
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th>Nombre</th>
+                        <th>Fecha Creación</th>
+                        <th>OPCIONES</th>
+                      </tr>
+                    </thead>
+                    <tbody >
+                      <tr v-for="(registro, index) in baseDatos" :key="index" >
+                        <th>
+                          <svg style="color: white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M224 0V128C224 145.7 238.3 160 256 160H384V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V64C0 28.65 28.65 0 64 0H224zM80 224C57.91 224 40 241.9 40 264V344C40 366.1 57.91 384 80 384H96C118.1 384 136 366.1 136 344V336C136 327.2 128.8 320 120 320C111.2 320 104 327.2 104 336V344C104 348.4 100.4 352 96 352H80C75.58 352 72 348.4 72 344V264C72 259.6 75.58 256 80 256H96C100.4 256 104 259.6 104 264V272C104 280.8 111.2 288 120 288C128.8 288 136 280.8 136 272V264C136 241.9 118.1 224 96 224H80zM175.4 310.6L200.8 325.1C205.2 327.7 208 332.5 208 337.6C208 345.6 201.6 352 193.6 352H168C159.2 352 152 359.2 152 368C152 376.8 159.2 384 168 384H193.6C219.2 384 240 363.2 240 337.6C240 320.1 231.1 305.6 216.6 297.4L191.2 282.9C186.8 280.3 184 275.5 184 270.4C184 262.4 190.4 256 198.4 256H216C224.8 256 232 248.8 232 240C232 231.2 224.8 224 216 224H198.4C172.8 224 152 244.8 152 270.4C152 287 160.9 302.4 175.4 310.6zM280 240C280 231.2 272.8 224 264 224C255.2 224 248 231.2 248 240V271.6C248 306.3 258.3 340.3 277.6 369.2L282.7 376.9C285.7 381.3 290.6 384 296 384C301.4 384 306.3 381.3 309.3 376.9L314.4 369.2C333.7 340.3 344 306.3 344 271.6V240C344 231.2 336.8 224 328 224C319.2 224 312 231.2 312 240V271.6C312 294.6 306.5 317.2 296 337.5C285.5 317.2 280 294.6 280 271.6V240zM256 0L384 128H256V0z" fill="green"></path></svg>
+                        </th>
+                        <td>{{ registro.basename }}</td>
+                        <td>{{ registro.fechaCreacion }}</td>
+                        <td class="text-center">
+                         <button type="button" class="btn btn-info " @click="verBaseDatos( registro )">
+                            Ver Base de Datos
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody> 
+                  </table>
+
+                  <!--
+
+                  <span v-show="procesando == false && registros.length == 0">Sin Información </span> 
+                  <span v-show="procesando == true">Procesando Información</span> 
+
+                  -->
+
+                </div>
+              </div>
+
               <!--Listado-->
               <div class="row" >
                 <div class="col-md-12">
+                  <h5 class="card-title">Listado de Registros en la Bases de Datos <b> {{ nombreBaseDatos == '' ? '' : '( ' + nombreBaseDatos + ' - ' + registros.length + ' registros )' }}  </b></h5>
                   <table class="table table-hover table-striped">
                     <thead>
                       <tr><th><input type="checkbox" v-model="checkedAll" @change="changeCheckAll"/></th><th>Id</th><th>NUMERO DE CONTROL</th><th>CURP</th><th>NOMBRE ENTIDAD</th><th>OPCIONES</th></tr>
@@ -138,6 +178,11 @@
                   <span v-show="procesando == false && registros.length == 0">Sin Información </span> 
                   <span v-show="procesando == true">Procesando Información</span> 
 
+                </div>
+              </div>
+              <div class="row" v-show="registros.length>0">
+                <div class="col-md-12">
+                  <h5>Total Registros : {{ registros.length }}</h5>
                 </div>
               </div>
               <!--Opciones-->
@@ -305,6 +350,8 @@
     },
     data () {
       return {
+        nombreBaseDatos: '',
+        baseDatos: [], 
         PDFVersion: 'reporte_3',
         vistaPDF: 'I',
         PDFVersiones: [ 'reporte', 'reporte_2' ],
@@ -400,6 +447,39 @@
       }
     },
     methods: {
+      verBaseDatos(baseDatos){
+        var self = this;
+        console.log(baseDatos)
+        self.procesando = true;
+        self.registros = [];
+        self.registrosData = [];
+        self.checkedAll = false
+
+        self.nombreBaseDatos = baseDatos.basename;
+
+        Vue.showLoader();
+        axios.get(process.env.VUE_APP_API_HOST_JS + '/dbcsv.php?db='+ baseDatos.filename )
+        .then(async resp => {
+          self.procesando = false;
+          Vue.hideLoader();
+          //console.log( resp) ;
+          //filtrar por entidad
+          if( resp.status == 200 ){
+            if( resp.data.DatosJSON ){
+              if( resp.data.DatosJSON.length > 0 ){
+                self.registrosData = resp.data.DatosJSON
+                self.filtroEntidad()
+              }
+            }
+          }
+        })
+        .catch(err => {
+          self.procesando = false;
+          Vue.hideLoader();
+          console.log( 'error' + err )
+        })
+
+      },
       getNombreEstado(registro){
         if( registro['NOM_ENT'] ){
           return registro['NOM_ENT']
@@ -412,6 +492,7 @@
         self.registros = [];
         self.registrosData = [];
         self.procesando = true;
+        self.nombreBaseDatos = '';
         Vue.showLoader();
 
         if( self.month == '0' || self.year == '0' ){
@@ -419,7 +500,27 @@
         }
 
         self.checkedAll = false
-        
+
+        //Verificar las bases de datos
+        var params = { 'ruta': '/dbcsv/', 'psPtrn': self.year + '_' + self.month + '*.csv' }
+        self.baseDatos = [];
+        Vue.archivos( params ).then(resp => {
+          self.procesando = false;
+          Vue.hideLoader();
+
+          console.log( resp )
+          self.baseDatos = resp
+
+          if( self.baseDatos.length > 0 ){
+            self.verBaseDatos(self.baseDatos[0])
+          }
+        })
+        .catch(function(error) {
+          //error en la consulta regresamos
+          
+        });
+
+        /*
         axios.get(process.env.VUE_APP_API_HOST_JS + '/dbcsv.php?month='+ self.month + '&year=' + self.year )
         .then(async resp => {
           self.procesando = false;
@@ -440,6 +541,8 @@
           Vue.hideLoader();
           console.log( 'error' + err )
         })
+
+        */
       },
       changeCheckAll(ev){
         this.registros.forEach(rgst=>{rgst.seleccionado = ev.target.checked ? '1': '0'})
@@ -636,7 +739,8 @@
         if(self.month == '03' ){ mes = 'Marzo'; }
         if(self.month == '04' ){ mes = 'Abril'; }
         if(self.month == '05' ){ mes = 'Mayo'; }
-        if(self.month == '06' ){ mes = 'Junio'; mes = 'Julio'; }
+        //if(self.month == '06' ){ mes = 'Junio'; mes = 'Julio'; }
+        if(self.month == '06' ){ mes = 'Junio'; }
         if(self.month == '07' ){ mes = 'Julio'; }
         if(self.month == '08' ){ mes = 'Agosto'; }
         if(self.month == '09' ){ mes = 'Septiembre'; }
